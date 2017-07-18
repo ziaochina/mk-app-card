c1f2df1085f5218fea011553bc20eecd2efb0e32
ziaochina <ziaochina@gmail.com>
2017-07-18 09:28:16 +0800

    zlj

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import { config, start, componentFactory } from 'mk-meta-engine'
+import myConfig  from './config'
+
+import mk_app_card from './apps/mk-app-card/index.js'
+
+const apps = {
+	[mk_app_card.name]:mk_app_card,	
+}
+
+
+config(myConfig({apps}))
+
+
+import * as mkComponents from 'mk-component'
+
+Object.keys(mkComponents).forEach(key=>{
+	componentFactory.registerComponent(key, mkComponents[key])
+})
+	
+
+start()
